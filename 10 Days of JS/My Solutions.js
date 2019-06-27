4c345f0c9d549e8d003501cdb814c68538722a94
kaurnavjeet <kaurnavjeet09@gmail.com>
2019-06-26 20:49:05 -0700

    solutions 6.26.19 continued

--- 10 Days of JS/My Solutions.js
+++ 10 Days of JS/My Solutions.js
@@ -107,3 +107,57 @@ function getCount(objects) {
   }
   return count;
 }
+
+// Task
+
+// Create a Polygon class that has the following properties:
+
+// A constructor that takes an array of integer values describing the lengths of the polygon's sides.
+// A perimeter() method that returns the polygon's perimeter.
+
+class Polygon {
+  constructor(s) {
+    this.side = s;
+  }
+
+  perimeter() {
+    let val = 0;
+    for (let i = 0; i < this.side.length; i++) {
+      val += this.side[i];
+    }
+    return val;
+  }
+}
+
+// Task
+
+// We provide the implementation for a Rectangle class in the editor. Perform the following tasks:
+
+// Add an area method to Rectangle's prototype.
+// Create a Square class that satisfies the following:
+// It is a subclass of Rectangle.
+// It contains a constructor and no other methods.
+// It can use the Rectangle class' area method to print the area of a Square object.
+
+class Rectangle {
+  constructor(w, h) {
+    this.w = w;
+    this.h = h;
+  }
+}
+
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function() {
+  return this.w * this.h;
+};
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+  constructor(s) {
+    super(s, s);
+  }
+}
